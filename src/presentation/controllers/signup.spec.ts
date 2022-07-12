dc65b2daf619c1936c6a593d195029df89603c81
tech3br <dfsdireito95@gmail.com>
2022-07-12 17:27:14 -0300

    refactor: make SignUpController conforms to a protocol

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -20,7 +20,7 @@ describe("Signup Controller", () => {
     const sut = new SignupController();
     const httpRequest = {
       body: {
-        name: "john@example.com",
+        name: "John",
         password: "123456",
         passwordConfirmation: "passwordConfirmation",
       },
@@ -28,5 +28,35 @@ describe("Signup Controller", () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("email"));
+  });
+
+  test("should return 400 if no password is provided", () => {
+    const sut = new SignupController();
+    const httpRequest = {
+      body: {
+        name: "John",
+        email: "john@example.com",
+        passwordConfirmation: "passwordConfirmation",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
+  });
+
+  test("should return 400 if no password confirmation is provided", () => {
+    const sut = new SignupController();
+    const httpRequest = {
+      body: {
+        name: "John",
+        email: "john@example.com",
+        password: "123456",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError("passwordConfirmation")
+    );
   });
 });
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,16 @@
 import { MissingParamError } from "../errors/missing-param-error";
 import { badRequest } from "../helpers/http-helper";
+import { Controller } from "../protocols/controller";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
-export class SignupController {
+export class SignupController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ["name", "email"];
+    const requiredFields = [
+      "name",
+      "email",
+      "password",
+      "passwordConfirmation",
+    ];
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
